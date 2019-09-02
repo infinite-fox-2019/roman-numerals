824b706f99cfaa134910c6a6f6256f680baea92c
laskarks <laskar.ksatria12@gmail.com>
2019-09-02 07:03:40 -0400

    roman-numeral

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,42 @@
-function to_roman (num) {
-  // your implementation code here
-}
+function to_roman(num) {
+    // your implementation code here
+    if (num > 3000) {
+        return 'Sorry this app cant do that'
+    } else {
+
+        let roma = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+        let arab = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        let pecah = [1000,100,10,1];
+        let numArray = [];
+        for (let i = 0;i<pecah.length;i++) {
+          if (num>=pecah[i]) {
+            let rek = Math.floor(num/pecah[i]);
+            numArray.push(rek*pecah[i])
+            num -= (rek*pecah[i])
+          }
+        }//end loop i
+      
+        let strNum = [];
+        for (let i = 0;i<numArray.length;i++) {
+          let rekString = ''
+          while (numArray[i]>0) {
+            for (let j = 0;j<arab.length;j++) {
+              if (numArray[i] >= arab[j]) {
+                rekString+=roma[j];
+                let rek = numArray[i];
+                rek = rek - arab[j];
+                numArray[i] = rek;
+              }
+            }//end loop j
+          }//endwhile
+          strNum.push(rekString)
+        }//end loop i     
+      
+      
+       return strNum.join('')
+    } //end else
+
+} //end function
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
