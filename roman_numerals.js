fc64933eb46c0549e44442cea55e7c0be80fb42a
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-02 18:38:19 +0700

    roman_numerals_nadhil

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,46 @@
 function to_roman (num) {
-  // your implementation code here
+  var hasil = ''
+ var convert = {
+   1 : 'I',
+   4 : 'IV',
+   5 : 'V',
+   9 : 'IX',
+   10 : 'X',
+   40 : 'XL',
+   50 : 'L',
+   90 : 'LC',
+   100 : 'C',
+   400 : 'CD',
+   500 : 'D',
+   900 : 'CM',
+   1000 : 'M'
+ }
+var angka = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+for (var i=0; i<angka.length;i++){
+  while (num >= angka[i]){
+    num = num - angka[i]
+    hasil = hasil + convert[angka[i]]
+  }
 }
+return hasil
+}
+/*
+1 = I
+2 = II
+3 = III
+4 = IV
+5 = V
+6 = VI
+7 = VII
+8 = VIII
+9 = IX
+10 = X
+11 = XI
+12 = XII
+49 = XL
+*/
+
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
