c9dae7f14d557b3c7e282eaae8e9e0a8f1db86aa
evans <karlinevans333@gmail.com>
2019-09-02 14:42:14 +0700

    Roman Numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,40 @@
 function to_roman (num) {
   // your implementation code here
+  // var arr = [1000, 500, 100, 50, 10, 5, 1]
+  var obj = {
+      'M' : 1000,
+      'D' : 500,
+      'C' : 100,
+      'L' : 50,
+      'X' : 10,
+      'V' : 5,
+      'I' : 1
+  }
+  var hasil = ""
+  if (num > 3000){
+      return "input maksimal 3000"
+  }
+  for(var i = 0 ; i < Object.keys(obj).length ; i++){
+      if(Math.floor(num / obj[Object.keys(obj)[i]]) != 0){
+          if(Math.floor(num / obj[Object.keys(obj)[i]]) == 4){
+              hasil += Object.keys(obj)[i]
+              hasil += Object.keys(obj)[i-1]
+              num = num % obj[Object.keys(obj)[i]]
+          }
+          else if(Math.floor(num / obj[Object.keys(obj)[i+1]]) == 9){
+              hasil += Object.keys(obj)[i+1]
+              hasil += Object.keys(obj)[i-1]
+              num = num % obj[Object.keys(obj)[i+1]]
+          }
+          else{
+              for(var j = 0 ; j < Math.floor(num / obj[Object.keys(obj)[i]]) ; j++){
+                  hasil += Object.keys(obj)[i]
+              }
+              num = num % obj[Object.keys(obj)[i]]
+          }
+      }
+  }
+  return hasil
 }
 
 // Drive code
