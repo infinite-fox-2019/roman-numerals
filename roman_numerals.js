2a326389de8ec281a9348332d7d481f211276f03
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-02 16:48:49 +0700

    :anchor:

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,33 @@
-function to_roman (num) {
+function to_roman(num) {
   // your implementation code here
+  if (!num) return ''
+
+  const roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  let result = ''
+
+  for (let key in roman) {
+    while (num >= roman[key]) {
+      num -= roman[key]
+      result += key
+    }
+  }
+
+  return result
 }
 
 // Drive code
