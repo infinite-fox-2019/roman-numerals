2073b8585a43a72f2ad3ff0e7bbeceae5562a3fc
arnold_therigan <arnoldtherigan15@gmail.com>
2019-09-02 16:29:28 +0700

    update roman_numerals.js

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,13 +1,43 @@
-function to_roman (num) {
-  // your implementation code here
+function toRoman (num) {
+  var numbersRoman = {
+      M: 1000,
+      D: 500,
+      CD: 400,
+      C: 100,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      VIII: 8,
+      VII: 7,
+      VI: 6,
+      V: 5,
+      IV: 4,
+      III: 3,
+      II: 2,
+      I: 1
+  }
+  var convertResult = '';
+  while(num > 0) {
+      for (var number in numbersRoman) {
+          if (num >= numbersRoman[number]) {
+              convertResult += number;
+              num -= numbersRoman[number];
+              break;
+          }
+      }
+  }
+  return convertResult;
 }
 
-// Drive code
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|———————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+
+console.log('My totally sweet testing script\n');
+console.log('input | expected | actual');
+console.log('_____ | ________ | ______');
+console.log('4     | IV       |', toRoman(4));
+console.log('9     | IX       |', toRoman(9));
+console.log('23    | XXIII    |', toRoman(23));
+console.log('1453  | MCDLII   |', toRoman(1453));
+console.log('1646  | MDCXLVI  |', toRoman(1646));
+console.log('33    | XXXIII    |', toRoman(33));
+
