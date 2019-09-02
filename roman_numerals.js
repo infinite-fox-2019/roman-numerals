2f5754bf366e3fa3bbe45ba96d486f7f5c0aeb70
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-02 16:02:42 +0700

    Menyelesaikan tugas Roman Numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,31 @@
 function to_roman (num) {
-  // your implementation code here
+  var convertionTable = [
+      ['I',1],
+      ['IV',4],
+      ['V',5],
+      ['IX',9],
+      ['X',10],
+      ['XL',40],
+      ['L',50],
+      ['XC',90],
+      ['C',100],
+      ['CD',400],
+      ['D',500],
+      ['CM',900],
+      ['M',1000],
+  ]
+  var output = ''
+  var countUp = 0
+  while(countUp<num){
+      for(var i=convertionTable.length-1;i>=0;i--){
+          if(convertionTable[i][1]<=num - countUp){
+              output += convertionTable[i][0]
+              countUp += convertionTable[i][1]
+              break
+          }
+      }
+  }
+  return output
 }
 
 // Drive code
