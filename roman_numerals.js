767e667e0808a9b58a5d469045e3c1056a432d4f
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-09-02 20:37:20 +0700

    selesai

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,17 @@
 function to_roman (num) {
   // your implementation code here
+  var hasil = ''
+  var ref = [[1000, 'M'], [900, 'CM'],[500, 'D'], [400, 'CD'], [100, 'C'], [50, 'L'],[40, 'XL'], [10, 'X'],[9, 'IX'], [5, 'V'],[4,'IV'], [1, 'I'] ]
+
+  for(var i = 0; i < ref.length; i++){
+      if(Math.floor(num / ref[i][0]) != 0){
+          for(var j = 0; j < Math.floor(num / ref[i][0]); j++){
+              hasil += ref[i][1]
+          }
+          num = num % ref[i][0]
+      }
+  }    
+  return hasil
 }
 
 // Drive code
