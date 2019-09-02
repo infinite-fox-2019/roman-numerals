d32daf2d24ad5d8bc1ffd814d7c85b35a310d85b
dipadana <putudipadana@gmail.com>
2019-09-02 16:55:30 +0700

    Update logic

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,3 +1,4 @@
+// Old
 function extendedNum(num){
   let stringNum = String(num);
   let output = [];
@@ -10,7 +11,7 @@ function extendedNum(num){
   return output;
 }
 
-function to_roman (num) {
+function to_romanOld(num) {
   var data = extendedNum(num);
   var output = '';
   for(var i = 0; i < data.length; i++){
@@ -81,6 +82,67 @@ function to_roman (num) {
     }
   }
   return output;
+}
+
+// REVISI new
+function to_roman(num){
+  var output = '';
+
+  while(num > 0){
+    if(num >= 1000){
+      output += 'M';
+      num -= 1000;
+    }
+    else if(num >= 900){
+      output += 'CM';
+      num -= 900;
+    }
+    else if(num >= 500){
+      output += 'D';
+      num -= 500;
+    }
+    else if(num >= 400){
+      output += 'CD';
+      num -= 400;
+    }
+    else if(num >= 100){
+      output += 'C';
+      num -= 100;
+    }
+    else if(num >= 90){
+      output += 'XC';
+      num -= 90;
+    }
+    else if(num >= 50){
+      output += 'L';
+      num -= 50;
+    }
+    else if(num >= 40){
+      output += 'XL';
+      num -= 40;
+    }
+    else if(num >= 10){
+      output += 'X';
+      num -= 10;
+    }
+    else if(num >= 9){
+      output += 'IX';
+      num -= 9;
+    }
+    else if(num >= 5){
+      output += 'V';
+      num -= 5;
+    }
+    else if(num >= 4){
+      output += 'IV';
+      num -= 4;
+    }
+    else{
+      output += 'I';
+      num -= 1;
+    }
+  }
+  return output
 }
 
 // Drive code
